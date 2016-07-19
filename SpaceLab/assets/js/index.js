990080cafef67b6ffa3941ca3f43e310b85918c4
WangRex <503431920@qq.com>
2016-07-19 11:34:17 +0800

    Add callback of game creation

--- SpaceLab/assets/js/index.js
+++ SpaceLab/assets/js/index.js
@@ -1,11 +1,28 @@
 var indexModule = (function(im) {
-	im.loadPage = function(pageName, callback) {
-		$("#main-content").load(pageName+".html", function() {
-			if(callback) {
-				callback();
-			}
-		});
-	}
+    im.loadPage = function(pageName, callback) {
+        $("#main-content").load(pageName + ".html", function() {
+            if (callback) {
+                callback();
+            }
+        });
+    }
+    im.addGame = function() {
+        var data = $("#addGameForm").serializeJson();
+        // console.log(data);
+        //{gameTime: "", gameRule: "1", gamePrize: "", gamePlace: "", gameName: "", gameBrief: ""}
+        $.ajax({
+            type: 'get',
+            dataType: "json",
+            url: 'assets/json/addGame.json',
+            data: data,
+            async: false,
+            success: function(result) {
+                // console.log(result);
+                $("#addGameContent").html(result.message);
+                $("#basicModal").modal('show');
+            }
+        });
+    }
     im.extendFunc = function() {
         console.log("This is the extend function!");
     }
