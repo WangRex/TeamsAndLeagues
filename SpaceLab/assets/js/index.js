0d976a5bffd509b05762f41920f2bacec0088d5c
WangRex <503431920@qq.com>
2016-07-19 15:32:07 +0800

    show center modal

--- SpaceLab/assets/js/index.js
+++ SpaceLab/assets/js/index.js
@@ -1,7 +1,7 @@
 var indexModule = (function(im) {
     im.loadPage = function(pageName, callback) {
         $("#main-content").load(pageName + ".html", function() {
-        	app.bread();
+            app.bread();
             if (callback) {
                 callback();
             }
@@ -20,9 +20,20 @@ var indexModule = (function(im) {
             async: false,
             success: function(result) {
                 // console.log(result);
+                $('.modal').on('show.bs.modal', im.fixModal);
+                $(window).on('resize', im.fixModal);
                 $("#addGameContent").html(result.message);
                 $("#basicModal").modal('show');
             }
+        });
+    }
+    im.fixModal = function() {
+        $('.modal').each(function(i) {
+            var $clone = $(this).clone().css('display', 'block').appendTo('body');
+            var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+            top = top > 0 ? top : 0;
+            $clone.remove();
+            $(this).find('.modal-content').css("margin-top", top);
         });
     }
     im.extendFunc = function() {
