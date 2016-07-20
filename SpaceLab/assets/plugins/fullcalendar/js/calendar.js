08ae217d2222543f1f128372cec1023dfb659a05
WangRex <503431920@qq.com>
2016-07-20 13:36:46 +0800

    seperate the game time to begain and end time add events.json update gameList.json update the fullcalendar update gameList.html

--- SpaceLab/assets/plugins/fullcalendar/js/calendar.js
+++ SpaceLab/assets/plugins/fullcalendar/js/calendar.js
@@ -33,31 +33,39 @@
         var m = date.getMonth();
         var y = date.getFullYear();
 
-        $('#calendar').fullCalendar({
-            dayClick: function() {
-                console.log($(this));
-            },
-            header: {
-                left: 'prev,next  today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            editable: true,
-            droppable: true,
-            events: [{
-                title: '大连 VS 皇马',
-                start: new Date(y, m, 1)
-            }, ],
-            eventClick: function(calEvent, jsEvent, view) {
+        $.ajax({
+            type: "get",
+            url: "assets/json/events.json",
+            dataType: "json",
+            async: false,
+            success: function(result) {
+                // console.log(result);
+                $('#calendar').fullCalendar({
+                    dayClick: function() {
+                        console.log($(this));
+                    },
+                    header: {
+                        left: 'prev,next  today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay'
+                    },
+                    editable: true,
+                    droppable: true,
+                    events: result,
+                    eventClick: function(calEvent, jsEvent, view) {
 
-                //alert('Event: ' + calEvent.title);
-                $("#eventsDetails").find("span").html(calEvent.title);
-                //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
-                //alert('View: ' + view.name);
+                        //alert('Event: ' + calEvent.title);
+                        $("#eventsDetails").find("span").html(calEvent.title);
+                        //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+                        //alert('View: ' + view.name);
 
-                // change the border color just for fun
-                //$(this).css('border-color', 'red');
+                        // change the border color just for fun
+                        //$(this).css('border-color', 'red');
 
+                    }
+                });
             }
         });
+
+
     });
