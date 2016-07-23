4f03ee9c8fc0f91e0eeb334535ad2fadf424025d
wangrex <503431920@qq.com>
2016-07-23 21:32:28 +0800

    add game and match details

--- SpaceLab/assets/js/index.js
+++ SpaceLab/assets/js/index.js
@@ -3,7 +3,7 @@ var indexModule = (function(im) {
         $("#main-content").load(pageName + ".html", function() {
             app.bread();
             if (callback) {
-                callback;
+                callback();
             }
 
         });
@@ -41,8 +41,108 @@ var indexModule = (function(im) {
         $(window).on('resize', im.fixModal);
         $("#" + modalId).modal('show');
     }
-    im.initDataTable = function(id) {
-        $("#" + id).dataTable();
+    im.loadGameDetailsPage = function(gameId) {
+        im.loadPage("gameDetails", im.loadGameDetails);
+
+    }
+    im.loadGameDetails = function() {
+        $.ajax({
+            type: 'get',
+            dataType: "json",
+            url: 'assets/json/gameInfo.json',
+            async: false,
+            success: function(result) {
+                var html = template('gameDetails-template', result.gameInfo);
+                $("#gameDetails").html(html);
+            }
+        });
+
+        var settings = {
+            "ajax": "assets/json/gameDetails.json",
+            "columns": [{
+                "data": "teamA"
+            }, {
+                "data": "teamB"
+            }, {
+                "data": "status"
+            }, {
+                "data": "result"
+            }, {
+                "data": "matchId"
+            }],
+            "columnDefs": [{
+                "targets": [2],
+                "render": function(data, type, full) {
+                    var str = "未进行";
+                    if (data == "1") {
+                        str = "已结束";
+                    }
+                    return str;
+                }
+            }, {
+                "targets": [3],
+                "render": function(data, type, full) {
+                    var str = "无比分";
+                    if (full.status == "1") {
+                        str = full.result;
+                    }
+                    return str;
+                }
+            }, {
+                "targets": [4],
+                "render": function(data, type, full) {
+                    var str = "<a href='javascript:void(0);' onclick='indexModule.getMatchDetails(" + '"matchDetailsModal"' + "," + full.matchId + ");'>查看</a>";
+                    return str;
+                }
+            }]
+        };
+
+        im.initDataTable("gameDetails-table", settings);
+    }
+
+    im.getMatchDetails = function(modalId, matchId) {
+        $.ajax({
+            type: 'get',
+            dataType: "json",
+            url: 'assets/json/matchDetails.json',
+            data: {matchId: matchId},
+            async: false,
+            success: function(result) {
+                var html = template('matchDetails-table-template', result);
+                $("#matchDetails-table").html(html);
+            }
+        });
+        im.popupModal(modalId);
+    }
+    im.initDataTable = function(id, settings) {
+        var defaultOptions = {
+            language: {
+                "sProcessing": "处理中...",
+                "sLengthMenu": "显示 _MENU_ 项结果",
+                "sZeroRecords": "没有匹配结果",
+                "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+                "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+                "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+                "sInfoPostFix": "",
+                "sSearch": "搜索:",
+                "sUrl": "",
+                "sEmptyTable": "表中数据为空",
+                "sLoadingRecords": "载入中...",
+                "sInfoThousands": ",",
+                "oPaginate": {
+                    "sFirst": "首页",
+                    "sPrevious": "上页",
+                    "sNext": "下页",
+                    "sLast": "末页"
+                },
+                "oAria": {
+                    "sSortAscending": ": 以升序排列此列",
+                    "sSortDescending": ": 以降序排列此列"
+                }
+            }
+        };
+        var options = $.extend({}, defaultOptions, settings);
+        $("#" + id).dataTable(options);
     }
     im.extendFunc = function() {
         console.log("This is the extend function!");
