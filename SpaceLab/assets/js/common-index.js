1fdc06f7bc651b91f9465ff2554207ba6b830f65
wangrex <503431920@qq.com>
2016-09-29 23:25:05 +0800

    比赛改成轮次

--- SpaceLab/assets/js/common-index.js
+++ SpaceLab/assets/js/common-index.js
@@ -45,8 +45,8 @@ var commonIndexModule = (function(cim) {
         var gameRule = $("#gameInfo").attr("data-gamerule");
         if (gameRule.indexOf("联赛") != -1) {
             params.groupName = "B";
-            var groupA = '<div class="row boardData boardTitle  groupA" id="boradDataA"><div class="col-xs-1">A组</div><div class="col-xs-3">球队</div><div class="col-xs-1">比赛</div><div class="col-xs-1">胜</div><div class="col-xs-1">平</div><div class="col-xs-1">负</div><div class="col-xs-1">进球数</div><div class="col-xs-1">失球数</div><div class="col-xs-1">净胜球</div><div class="col-xs-1">积分</div></div>';
-            var groupB = '<div class="row boardData boardTitle  groupA" id="boradDataB"><div class="col-xs-1">B组</div><div class="col-xs-3">球队</div><div class="col-xs-1">比赛</div><div class="col-xs-1">胜</div><div class="col-xs-1">平</div><div class="col-xs-1">负</div><div class="col-xs-1">进球数</div><div class="col-xs-1">失球数</div><div class="col-xs-1">净胜球</div><div class="col-xs-1">积分</div></div>';
+            var groupA = '<div class="row boardData boardTitle  groupA" id="boradDataA"><div class="col-xs-1">A组</div><div class="col-xs-3">球队</div><div class="col-xs-1">轮次</div><div class="col-xs-1">胜</div><div class="col-xs-1">平</div><div class="col-xs-1">负</div><div class="col-xs-1">进球数</div><div class="col-xs-1">失球数</div><div class="col-xs-1">净胜球</div><div class="col-xs-1">积分</div></div>';
+            var groupB = '<div class="row boardData boardTitle  groupA" id="boradDataB"><div class="col-xs-1">B组</div><div class="col-xs-3">球队</div><div class="col-xs-1">轮次</div><div class="col-xs-1">胜</div><div class="col-xs-1">平</div><div class="col-xs-1">负</div><div class="col-xs-1">进球数</div><div class="col-xs-1">失球数</div><div class="col-xs-1">净胜球</div><div class="col-xs-1">积分</div></div>';
             $("#boradData").after(groupB);
             $("#boradData").after(groupA);
             var fillinParams = { tmplId: 'scoreDetails-template', target: $(".groupB"), way: "after" };
@@ -55,7 +55,7 @@ var commonIndexModule = (function(cim) {
             fillinParams = { tmplId: 'scoreDetails-template', target: $(".groupA"), way: "after" };
             globalModule.globalAjax(globalModule.globalHomeUrl + "api/MatchScore/getAllMatchScore", params, globalModule.fillinInfoFromTmpl, null, null, null, fillinParams);
         } else {
-            var group = '<div class="row boardData boardTitle" id="boradDataGroup"><div class="col-xs-1">排名</div><div class="col-xs-3">球队</div><div class="col-xs-1">比赛</div><div class="col-xs-1">胜</div><div class="col-xs-1">平</div><div class="col-xs-1">负</div><div class="col-xs-1">进球数</div><div class="col-xs-1">失球数</div><div class="col-xs-1">净胜球</div><div class="col-xs-1">积分</div></div>';
+            var group = '<div class="row boardData boardTitle" id="boradDataGroup"><div class="col-xs-1">排名</div><div class="col-xs-3">球队</div><div class="col-xs-1">轮次</div><div class="col-xs-1">胜</div><div class="col-xs-1">平</div><div class="col-xs-1">负</div><div class="col-xs-1">进球数</div><div class="col-xs-1">失球数</div><div class="col-xs-1">净胜球</div><div class="col-xs-1">积分</div></div>';
             $("#boradData").after(group);
             var fillinParams = { tmplId: 'scoreDetails-template', target: $("#boradDataGroup"), way: "after" };
             globalModule.globalAjax(globalModule.globalHomeUrl + "api/MatchScore/getAllMatchScore", params, globalModule.fillinInfoFromTmpl, null, null, null, fillinParams);
