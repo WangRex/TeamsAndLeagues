5be2c531747bc5158773465204af3c89525ed643
WangRex <503431920@qq.com>
2016-09-30 16:10:03 +0800

    update common game result.

--- SpaceLab/assets/js/common-index.js
+++ SpaceLab/assets/js/common-index.js
@@ -108,8 +108,7 @@ var commonIndexModule = (function(cim) {
     }
     cim.viewGameResultPage = function(ttId, mainTeamId, subTeamId) {
         var params = { ttId: ttId, mainTeamId: mainTeamId, subTeamId: subTeamId };
-        // globalModule.loadPage("main-content", "gameResultPage", cim.viewGameResultPageInit, params);
-        window.open(window.location.href + "gameResultPageShare.html?ttId=" + ttId);
+        window.open("http://www.leyisports.com/gameResultPageShare.html?ttId=" + ttId);
     }
     cim.viewGameResultPageInit = function(params) {
         $("#gameResultPage-copy-right").hide();
